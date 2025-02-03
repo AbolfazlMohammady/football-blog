5b499445a0a878b1fa0f7d6c4fa8d1ca7947e8e4
AbolfazlMohammady <abolfazlking22@gmail.com>
2025-02-03 09:15:54 -0800

    forms account

---
+++ accounts/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import CustomUser
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import get_user_model
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'username',)
+        
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('email',  'username', )
+
+
+
+class UserProfileForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'email'] 
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form__input'}),
+            'last_name': forms.TextInput(attrs={'class': 'form__input'}),
+            'email': forms.EmailInput(attrs={'class': 'form__input'}),
+        }
