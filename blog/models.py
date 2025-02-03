e4212f7bf23cb417a0f6cd551681eaed39370302
AbolfazlMohammady <abolfazlking22@gmail.com>
2025-02-03 09:17:31 -0800

    models blog

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
